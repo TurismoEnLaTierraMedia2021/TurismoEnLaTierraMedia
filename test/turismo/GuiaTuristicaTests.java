1ff68997c4305aca168b669b579ca8a834adb4c6
Ariel Merlo <ariel_merlo_90@hotmail.com>
2021-08-27 15:48:06 -0300

    Se agrega test

---
+++ test/turismo/GuiaTuristicaTests.java
@@ -0,0 +1,14 @@
+package turismo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GuiaTuristicaTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

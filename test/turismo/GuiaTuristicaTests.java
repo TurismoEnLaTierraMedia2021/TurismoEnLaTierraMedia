98f0ecbdaf7d1ad98b71fbfdd2712cb90d8bf0c4
Ariel Merlo <ariel_merlo_90@hotmail.com>
2021-09-03 16:45:45 -0300

    Ahora con tests

--- test/turismo/GuiaTuristicaTests.java
+++ test/turismo/GuiaTuristicaTests.java
@@ -2,13 +2,31 @@ package turismo;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class GuiaTuristicaTests {
+	List<Usuario> usuarios;
+	List<Atraccion> atracciones;
+	List<Promocion> promociones;
+
+	@Before
+	public void setUp() {
+		usuarios = FileManager.getUsuarios();
+
+		atracciones = FileManager.getAtracciones();
+
+		promociones = FileManager.getPromociones();
+	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		assertEquals(22.40, promociones.get(0).calcularDescuento(),0.01);
+		assertEquals(36, promociones.get(1).calcularDescuento(),0.01);
+		assertEquals(10, promociones.get(2).calcularDescuento(),0.01);
 	}
 
 }

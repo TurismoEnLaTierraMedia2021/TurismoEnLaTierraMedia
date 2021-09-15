9433358b864631222db9ef4e14965fd924568a5d
Ariel Merlo <ariel_merlo_90@hotmail.com>
2021-09-15 09:46:08 -0300

    Agregadas las pruebas que sugirio Ignacio.

--- test/turismo/GuiaTuristicaTests.java
+++ test/turismo/GuiaTuristicaTests.java
@@ -21,8 +21,11 @@ public class GuiaTuristicaTests {
 		promociones = FileManager.getPromociones(atracciones);
 
 		vendibles = FileManager.getVendibles();
+		
 	}
 
+	//Al final del archivo se encuentra la lista de vendibles comentada.
+	
 	@Test
 	public void precioTest() {
 
@@ -105,5 +108,53 @@ public class GuiaTuristicaTests {
 		assertEquals(promociones.get(0).getTipo(), atracciones.get(0).getTipo());
 		assertEquals(usuarios.get(0).getTipoDeAtraccion(), atracciones.get(0).getTipo());
 	}
+	
+	@Test
+	public void ignaTest() {
+		assertTrue(usuarios.get(4).puedeComprar(vendibles.get(13)));
+
+		usuarios.get(4).comprarVendible(vendibles.get(13));
+
+		assertTrue(usuarios.get(4).yaLoCompro(vendibles.get(8)));
+		assertFalse(usuarios.get(4).puedeComprar(vendibles.get(8)));
+		
+	}
+	@Test
+	public void igna2Test() {
+		assertTrue(usuarios.get(4).puedeComprar(vendibles.get(11)));
+
+		usuarios.get(4).comprarVendible(vendibles.get(11));
+
+		assertTrue(usuarios.get(4).yaLoCompro(vendibles.get(6)));
+		assertFalse(usuarios.get(4).puedeComprar(vendibles.get(14)));
+		
+	}
 
 }
+
+//Lista de vendibles
+
+//0	 [Atraccion [nombre=Moria, costo=10.0, duracion=2.0, tipoDeAtraccion=AVENTURA, cupo=6]
+//1 , Atraccion [nombre=Minas Tirith, costo=5.0, duracion=2.5, tipoDeAtraccion=PAISAJE, cupo=25]
+//2 , Atraccion [nombre=La Comarca, costo=3.0, duracion=6.5, tipoDeAtraccion=DEGUSTACION, cupo=150]
+//3 , Atraccion [nombre=Mordor, costo=25.0, duracion=3.0, tipoDeAtraccion=AVENTURA, cupo=4]
+//4 , Atraccion [nombre=Abismo de Helm, costo=5.0, duracion=2.0, tipoDeAtraccion=PAISAJE, cupo=15]
+//5 , Atraccion [nombre=Lothlorien, costo=35.0, duracion=1.0, tipoDeAtraccion=DEGUSTACION, cupo=30]
+//6 , Atraccion [nombre=Erebor, costo=12.0, duracion=3.0, tipoDeAtraccion=PAISAJE, cupo=32]
+//7 , Atraccion [nombre=Bosque Negro, costo=3.0, duracion=4.0, tipoDeAtraccion=AVENTURA, cupo=12]
+//8 , Atraccion [nombre=Taberna Pony Pisador, costo=17.0, duracion=5.0, tipoDeAtraccion=DEGUSTACION, cupo=18]
+//9 , Promocion AVENTURA [Valor: 22.4, Tiempo necesario: 7.0, Tipo de promocion:Descuento, Nombre del pack: Pack aventura,
+//	 Atracciones:Bosque Negro, Mordor]
+//10 , Promocion DEGUSTACION [Valor: 36.0, Tiempo necesario: 7.5, Tipo de promocion:Absoluta, Nombre del pack: Pack degustacion,
+//	 Atracciones:Lothlorien, La Comarca]
+//11 , Promocion PAISAJE [Valor: 10.0, Tiempo necesario: 7.5, Tipo de promocion:AxB, Nombre del pack: Pack paisajes,
+//	 Atracciones:Minas Tirith, Abismo de Helm, Erebor]
+//	 [Atraccion gratis: Erebor]
+//12 , Promocion AVENTURA [Valor: 30.0, Tiempo necesario: 5.0, Tipo de promocion:Absoluta, Nombre del pack: Pack locuras,
+//	 Atracciones:Moria, Mordor]
+//13 , Promocion DEGUSTACION [Valor: 38.0, Tiempo necesario: 12.5, Tipo de promocion:AxB, Nombre del pack: Pack festin,
+//	 Atracciones:La Comarca, Lothlorien, Taberna Pony Pisador]
+//	 [Atraccion gratis: Taberna Pony Pisador]
+//14 , Promocion PAISAJE [Valor: 9.35, Tiempo necesario: 5.0, Tipo de promocion:Descuento, Nombre del pack: Pack hotSale,
+//	 Atracciones:Erebor, Abismo de Helm]
+//]
